05a69d26302542d52ab1196f18f63101bac310d0
David Blass <ssalbdivad@gmail.com>
2020-07-01 21:42:21 -0400

    Migrate shell utilities to @re-do/utils.

--- src/utils.ts
+++
@@ -1,5 +0,0 @@
-import { shell as runShell, ShellOptions } from "@re-do/utils/dist/node"
-
-export const shell = runShell
-export const $ = (cmd: string, options?: ShellOptions) => () =>
-    shell(cmd, options)
